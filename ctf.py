cd119cceecac985643bb35e7cd18248d7f835ae4
mervan1337 <merpa433@student.liu.se>
2021-11-01 15:10:43 +0100

    11-01 Första filerna

---
+++ ctf.py
@@ -0,0 +1,128 @@
+import pygame
+from pygame.locals import *
+from pygame.color import *
+import pymunk
+
+#----- Initialisation -----#
+
+#-- Initialise the display
+pygame.init()
+pygame.display.set_mode()
+
+#-- Initialise the clock
+clock = pygame.time.Clock()
+
+#-- Initialise the physics engine
+space = pymunk.Space()
+space.gravity = (0.0,  0.0)
+space.damping = 0.1 # Adds friction to the ground for all objects
+
+
+#-- Import from the ctf framework
+import ai
+import images
+import gameobjects
+import maps
+
+#-- Constants
+FRAMERATE = 50
+
+#-- Variables
+#   Define the current level
+current_map         = maps.map0
+#   List of all game objects
+game_objects_list   = []
+tanks_list          = []
+
+#-- Resize the screen to the size of the current level
+screen = pygame.display.set_mode(current_map.rect().size)
+
+#<INSERT GENERATE BACKGROUND>
+#-- Generate the background
+background = pygame.Surface(screen.get_size())
+
+#   Copy the grass tile all over the level area
+for x in range(0, current_map.width):
+    for y in range(0,  current_map.height):
+        # The call to the function "blit" will copy the image
+        # contained in "images.grass" into the "background"
+        # image at the coordinates given as the second argument
+        background.blit(images.grass,  (x*images.TILE_SIZE, y*images.TILE_SIZE))
+
+
+#<INSERT CREATE BOXES>
+#-- Create the boxes
+for x in range(0, current_map.width):
+    for y in range(0,  current_map.height):
+        # Get the type of boxes
+        box_type  = current_map.boxAt(x, y)
+        # If the box type is not 0 (aka grass tile), create a box
+        if(box_type != 0):
+            # Create a "Box" using the box_type, aswell as the x,y coordinates,
+            # and the pymunk space
+            box = gameobjects.get_box_with_type(x, y, box_type, space)
+            game_objects_list.append(box)
+
+#<INSERT CREATE TANKS>
+#-- Create the tanks
+# Loop over the starting poistion
+for i in range(0, len(current_map.start_positions)):
+    # Get the starting position of the tank "i"
+    pos = current_map.start_positions[i]
+    # Create the tank, images.tanks contains the image representing the tank
+    tank = gameobjects.Tank(pos[0], pos[1], pos[2], images.tanks[i], space)
+    # Add the tank to the list of tanks
+    tanks_list.append(tank)
+
+#<INSERT CREATE FLAG>
+#-- Create the flag
+flag = gameobjects.Flag(current_map.flag_position[0], current_map.flag_position[1])
+
+#----- Main Loop -----#
+
+#-- Control whether the game run
+running = True
+
+skip_update = 0
+
+while running:
+    #-- Handle the events
+    for event in pygame.event.get():
+        # Check if we receive a QUIT event (for instance, if the user press the
+        # close button of the wiendow) or if the user press the escape key.
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            running = False
+    #-- Update physics
+    if skip_update == 0:
+        # Loop over all the game objects and update their speed in function of their
+        # acceleration.
+        for obj in game_objects_list:
+            obj.update()
+        skip_update = 2
+    else:
+        skip_update -= 1
+
+    #   Check collisions and update the objects position
+    space.step(1 / FRAMERATE)
+
+    #   Update object that depends on an other object position (for instance a flag)
+    for obj in game_objects_list:
+        obj.post_update()
+
+    #-- Update Display
+
+    #<INSERT DISPLAY BACKGROUND>
+
+    # Display the background on the screen
+    screen.blit(background, (0, 0))
+
+    #<INSERT DISPLAY OBJECTS>
+    # Update the display of the game objects on the screen
+    for obj in game_objects_list:
+        obj.update_screen(screen)
+
+    #   Redisplay the entire screen (see double buffer technique)
+    pygame.display.flip()
+
+    #   Control the game framerate
+    clock.tick(FRAMERATE)
